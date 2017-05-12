41ddc717b43e392f6e7f177309f253e0bb8eec73
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-05-12 14:36:47 -0700

    Run gulp format

--- test/index.js
+++ test/index.js
@@ -9,71 +9,73 @@ var sinon = require('sinon');
 var vfs = require('vinyl-fs');
 
 describe('Metal Tools - Build AMD', function() {
-  var originalCwd = process.cwd();
+	var originalCwd = process.cwd();
 
-  before(function() {
-    process.chdir('test/fixtures');
-  });
+	before(function() {
+		process.chdir('test/fixtures');
+	});
 
-  beforeEach(function() {
-    babelDeps.clearCache();
-  });
+	beforeEach(function() {
+		babelDeps.clearCache();
+	});
 
-  after(function() {
-    process.chdir(originalCwd);
-  });
+	after(function() {
+		process.chdir(originalCwd);
+	});
 
-  describe('Default src/dest', function() {
-    beforeEach(function() {
-      var pipe = {
-        pipe: function() {
-          return pipe;
-        }
-      };
-      sinon.stub(vfs, 'src').returns(pipe);
-      sinon.stub(vfs, 'dest');
-    });
+	describe('Default src/dest', function() {
+		beforeEach(function() {
+			var pipe = {
+				pipe: function() {
+					return pipe;
+				},
+			};
+			sinon.stub(vfs, 'src').returns(pipe);
+			sinon.stub(vfs, 'dest');
+		});
 
-    afterEach(function() {
-      vfs.src.restore();
-      vfs.dest.restore();
-    });
+		afterEach(function() {
+			vfs.src.restore();
+			vfs.dest.restore();
+		});
 
-  	it('should compile soy files from "src" and to "build/amd" by default', function() {
-      metalToolsBuildAmd();
-      assert.strictEqual('src/**/*.js', vfs.src.args[0][0]);
-      assert.strictEqual('build/amd', vfs.dest.args[0][0]);
-  	});
-  });
+		it('should compile soy files from "src" and to "build/amd" by default', function() {
+			metalToolsBuildAmd();
+			assert.strictEqual('src/**/*.js', vfs.src.args[0][0]);
+			assert.strictEqual('build/amd', vfs.dest.args[0][0]);
+		});
+	});
 
-  describe('Integration', function() {
-    beforeEach(function(done) {
-      deleteBuiltFiles(done);
-    });
+	describe('Integration', function() {
+		beforeEach(function(done) {
+			deleteBuiltFiles(done);
+		});
 
-  	after(function(done) {
-      deleteBuiltFiles(done);
-  	});
+		after(function(done) {
+			deleteBuiltFiles(done);
+		});
 
-  	it('should compile specified soy files to multiple AMD modules and source maps', function(done) {
-      var stream = metalToolsBuildAmd({
-        base: process.cwd(),
-        src: 'src/js/foo.js',
-        dest: 'build'
-      });
-      stream.on('end', function() {
-        assert.ok(fs.existsSync('build/metal/src/js/foo.js'));
-        assert.ok(fs.existsSync('build/metal/src/js/foo.js.map'));
-        assert.ok(fs.existsSync('build/dep/dep.js'));
-        assert.ok(fs.existsSync('build/dep/dep.js.map'));
-    		done();
-      });
-  	});
-  });
+		it('should compile specified soy files to multiple AMD modules and source maps', function(
+			done,
+		) {
+			var stream = metalToolsBuildAmd({
+				base: process.cwd(),
+				src: 'src/js/foo.js',
+				dest: 'build',
+			});
+			stream.on('end', function() {
+				assert.ok(fs.existsSync('build/metal/src/js/foo.js'));
+				assert.ok(fs.existsSync('build/metal/src/js/foo.js.map'));
+				assert.ok(fs.existsSync('build/dep/dep.js'));
+				assert.ok(fs.existsSync('build/dep/dep.js.map'));
+				done();
+			});
+		});
+	});
 });
 
 function deleteBuiltFiles(done) {
-  del('build').then(function() {
-    done();
-  });
+	del('build').then(function() {
+		done();
+	});
 }
--- test/lib/pipelines/buildAmd.js
+++ test/lib/pipelines/buildAmd.js
@@ -23,154 +23,171 @@ describe('Pipeline - Build AMD', function() {
 		process.chdir(originalCwd);
 	});
 
-	it('should build js files to multiple AMD modules and their source maps', function(done) {
-		var stream = vfs.src('src/js/foo.js')
-      .pipe(buildAmd({base: path.resolve()}));
+	it('should build js files to multiple AMD modules and their source maps', function(
+		done,
+	) {
+		var stream = vfs
+			.src('src/js/foo.js')
+			.pipe(buildAmd({base: path.resolve()}));
 
-    var files = [];
-    stream.on('data', function(file) {
+		var files = [];
+		stream.on('data', function(file) {
 			files.push(file.relative);
 		});
 		stream.on('end', function() {
 			assert.strictEqual(4, files.length);
 			assert.deepEqual(
-        [
-          'dep/dep.js',
-          'dep/dep.js.map',
-          'metal/src/js/foo.js',
-          'metal/src/js/foo.js.map'
-        ],
-        files.sort()
-      );
+				[
+					'dep/dep.js',
+					'dep/dep.js.map',
+					'metal/src/js/foo.js',
+					'metal/src/js/foo.js.map',
+				],
+				files.sort(),
+			);
 			done();
 		});
 		consume(stream);
 	});
 
-	it('should build js files to multiple AMD modules without their source maps', function(done) {
-		var stream = vfs.src('src/js/foo.js')
-      .pipe(buildAmd({
+	it('should build js files to multiple AMD modules without their source maps', function(
+		done,
+	) {
+		var stream = vfs.src('src/js/foo.js').pipe(
+			buildAmd({
 				base: path.resolve(),
-				sourceMaps: false
-			}));
+				sourceMaps: false,
+			}),
+		);
 
-    var files = [];
-    stream.on('data', function(file) {
+		var files = [];
+		stream.on('data', function(file) {
 			files.push(file.relative);
 		});
 		stream.on('end', function() {
 			assert.strictEqual(2, files.length);
 			assert.deepEqual(
-        [
-          'dep/dep.js',
-          'metal/src/js/foo.js'
-        ],
-        files.sort()
-      );
+				['dep/dep.js', 'metal/src/js/foo.js'],
+				files.sort(),
+			);
 			done();
 		});
 		consume(stream);
 	});
 
-	it('should build js files correctly when base folder doesn\'t contain node_modules', function(done) {
-		var stream = vfs.src('src/js/foo.js')
-      .pipe(buildAmd({base: path.resolve('src')}));
+	it("should build js files correctly when base folder doesn't contain node_modules", function(
+		done,
+	) {
+		var stream = vfs
+			.src('src/js/foo.js')
+			.pipe(buildAmd({base: path.resolve('src')}));
 
-    var files = [];
-    stream.on('data', function(file) {
+		var files = [];
+		stream.on('data', function(file) {
 			files.push(file.relative);
 		});
 		stream.on('end', function() {
 			assert.strictEqual(4, files.length);
 			assert.deepEqual(
-        [
-          'dep/dep.js',
-          'dep/dep.js.map',
-          'metal/js/foo.js',
-          'metal/js/foo.js.map'
-        ],
-        files.sort()
-      );
+				[
+					'dep/dep.js',
+					'dep/dep.js.map',
+					'metal/js/foo.js',
+					'metal/js/foo.js.map',
+				],
+				files.sort(),
+			);
 			done();
 		});
 		consume(stream);
 	});
 
-	it('should use given moduleName for the original source files', function(done) {
-		var stream = vfs.src('src/js/foo.js')
-      .pipe(buildAmd({base: path.resolve(), moduleName: 'foo'}));
+	it('should use given moduleName for the original source files', function(
+		done,
+	) {
+		var stream = vfs
+			.src('src/js/foo.js')
+			.pipe(buildAmd({base: path.resolve(), moduleName: 'foo'}));
 
-    var files = [];
-    stream.on('data', function(file) {
+		var files = [];
+		stream.on('data', function(file) {
 			files.push(file.relative);
 		});
 		stream.on('end', function() {
 			assert.strictEqual(4, files.length);
 			assert.deepEqual(
-        [
-          'dep/dep.js',
-          'dep/dep.js.map',
-          'foo/src/js/foo.js',
-          'foo/src/js/foo.js.map'
-        ],
-        files.sort()
-      );
+				[
+					'dep/dep.js',
+					'dep/dep.js.map',
+					'foo/src/js/foo.js',
+					'foo/src/js/foo.js.map',
+				],
+				files.sort(),
+			);
 			done();
 		});
 		consume(stream);
 	});
 
 	it('should normalize module path separators', function(done) {
-		var stream = vfs.src('src/js/foo.js')
-      .pipe(buildAmd({base: path.resolve(), moduleName: 'foo\\bar'}));
+		var stream = vfs
+			.src('src/js/foo.js')
+			.pipe(buildAmd({base: path.resolve(), moduleName: 'foo\\bar'}));
 
-    var files = [];
-    stream.on('data', function(file) {
+		var files = [];
+		stream.on('data', function(file) {
 			files.push(file.relative);
 		});
 		stream.on('end', function() {
 			assert.strictEqual(4, files.length);
 			assert.deepEqual(
-        [
-          'dep/dep.js',
-          'dep/dep.js.map',
-          'foo/bar/src/js/foo.js',
-          'foo/bar/src/js/foo.js.map'
-        ],
-        files.sort()
-      );
+				[
+					'dep/dep.js',
+					'dep/dep.js.map',
+					'foo/bar/src/js/foo.js',
+					'foo/bar/src/js/foo.js.map',
+				],
+				files.sort(),
+			);
 			done();
 		});
 		consume(stream);
 	});
 
-	it('should use import path as module id when it has the "module:" prefix', function(done) {
-    // Supress error due to missing imported file.
-    sinon.stub(console, 'warn');
+	it('should use import path as module id when it has the "module:" prefix', function(
+		done,
+	) {
+		// Supress error due to missing imported file.
+		sinon.stub(console, 'warn');
 
-		var stream = vfs.src('src/js/moduleAlias.js')
-      .pipe(buildAmd({base: path.resolve()}));
+		var stream = vfs
+			.src('src/js/moduleAlias.js')
+			.pipe(buildAmd({base: path.resolve()}));
 
-    stream.on('data', function(file) {
-      if (file.relative === 'metal/src/js/moduleAlias.js') {
-        var contents = file.contents.toString();
-        assert.notStrictEqual(-1, contents.indexOf('define([\'myModuleId\']'));
-        console.warn.restore();
-        done();
-      }
+		stream.on('data', function(file) {
+			if (file.relative === 'metal/src/js/moduleAlias.js') {
+				var contents = file.contents.toString();
+				assert.notStrictEqual(
+					-1,
+					contents.indexOf("define(['myModuleId']"),
+				);
+				console.warn.restore();
+				done();
+			}
 		});
 	});
 
-  it('should preserve relative paths as module ids', function(done) {
-    var stream = vfs.src('src/js/relativeImport.js')
-      .pipe(buildAmd({base: path.resolve()}));
+	it('should preserve relative paths as module ids', function(done) {
+		var stream = vfs
+			.src('src/js/relativeImport.js')
+			.pipe(buildAmd({base: path.resolve()}));
 
-    stream.on('data', function(file) {
-      if (file.relative === 'metal/src/js/relativeImport.js') {
-        var contents = file.contents.toString();
-        assert.notStrictEqual(-1, contents.indexOf('define([\'./foo\']'));
-        done();
-      }
-    });
-  });
+		stream.on('data', function(file) {
+			if (file.relative === 'metal/src/js/relativeImport.js') {
+				var contents = file.contents.toString();
+				assert.notStrictEqual(-1, contents.indexOf("define(['./foo']"));
+				done();
+			}
+		});
+	});
 });
